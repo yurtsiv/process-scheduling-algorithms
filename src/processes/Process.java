8092c16958f0d1dbfef08f289cbd99dbcd094f0f
stepy <yurtsiv.stepan@gmail.com>
2019-03-03 16:42:36 +0100

    remove .idea

--- src/processes/Process.java
+++ src/processes/Process.java
@@ -1,4 +1,14 @@
 package processes;
 
 public class Process {
+    private int id, estimatedRunningTime, startTime, runningProgress = 0;
+    boolean isFinished = false, isRunning = false;
+
+    public Process(int id, int estimatedRunningTime, int startTime) {
+        this.id = id;
+        this.estimatedRunningTime = estimatedRunningTime;
+        this.startTime = startTime;
+    }
+
+
 }
